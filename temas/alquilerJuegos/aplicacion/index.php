102d8798104753aa40e6f18caf5dc04f235b30d0
DWES <DWES@10.0.2.15>
2022-12-07 10:38:58 +0100

    Actualizacion Ejercicios

---
+++ temas/alquilerJuegos/aplicacion/index.php
@@ -0,0 +1,76 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+        <meta name="viewport" content="view=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <title>Juego Comares</title>
+    </head>
+    <body>
+        <div class="container mt-1">
+            <div class="divLog">
+                <h1>Juego Comares</h1>
+                <form action="" method="POST">
+                    Dni: <input type="text" name="dni" id="dni">
+                    Contraseña: <input type="password" name="pass" id="pass">
+                    <input type="button" name="login" value="Login" id="login">
+                </form>
+            </div>
+            <div class="divTabla mt-5">
+                <table class="table">
+                    <thead>
+                        <tr class="bg-dark text-light p-5">
+                            <th>Carátula</th>
+                            <th>Nombre Juego</th>
+                            <th>Nombre Consola</th>
+                            <th>Año</th>
+                            <th>Precio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>IMAGEN</td>
+                            <td>NOMBRE</td>
+                            <td>CONSOLA</td>
+                            <td>AÑO</td>
+                            <td>PRECIO</td>
+                        </tr> 
+                    
+                        <tr>
+                            <td>IMAGEN</td>
+                            <td>NOMBRE</td>
+                            <td>CONSOLA</td>
+                            <td>AÑO</td>
+                            <td>PRECIO</td>
+                        </tr> 
+                    
+                        <tr>
+                            <td>IMAGEN</td>
+                            <td>NOMBRE</td>
+                            <td>CONSOLA</td>
+                            <td>AÑO</td>
+                            <td>PRECIO</td>
+                        </tr> 
+                    </tbody>
+                    
+                </table>
+            </div>
+        </div>
+    </body>
+</html>
+
+
+
+
+
+<?php
+    
+
+
+
+
+
+
+
+?>
+
